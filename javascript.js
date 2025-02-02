b03f55e916283fa4e76f80e2f37fa1a7c9868cb1
loops <128599880+loopsc@users.noreply.github.com>
2025-02-02 15:40:50 +1300

    Added coloring to win or lose and rounds played
    If the player wins a round, the text will display green else red or black if draw.
    Box with round number displayed at top of screen.
    Changed background colour.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0
 
 // Returns the choice from the computer as a string
 function getComputerChoice() {
   randomNumber = Math.random();
-  testingNumber = 0.9;
+  // testingNumber = 0.9;
 
   if (randomNumber <= 0.33) {
     botResult.textContent = 'Computer chose: Rack'
@@ -24,6 +25,7 @@ const botResult = document.querySelector('#bot-result')
 const buttons = document.querySelector('#button-container')
 const playerScore = document.querySelector('#player-score')
 const compScore = document.querySelector('#comp-score')
+const round = document.querySelector('#round')
 
 
 buttons.addEventListener('click', (e) => {
@@ -33,50 +35,65 @@ buttons.addEventListener('click', (e) => {
       playRound('rock',getComputerChoice())
       playerScore.textContent = humanScore;
       compScore.textContent = computerScore;
+      round.textContent = rounds;
       break
     case 'paper-btn':
       playRound('paper',getComputerChoice())
       playerScore.textContent = humanScore;
       compScore.textContent = computerScore;
+      round.textContent = rounds;
       break
     case 'scissors-btn':
       playRound('scissors',getComputerChoice())
       playerScore.textContent = humanScore;
       compScore.textContent = computerScore;
+      round.textContent = rounds;
       break
   }
 })
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == "rock") {
+    rounds += 1
     if (computerChoice == "rock") {
       result.textContent = 'Draw'
+      result.style.color = 'black'
     } else if (computerChoice == "paper") {
       computerScore++;
       result.textContent = "Unluggy. Pahper beats Rack";
+      result.style.color = 'red'
     } else {
       humanScore++;
       result.textContent = "Noice! Rack beats Skazzors";
+      result.style.color = 'green'
     }
   } else if (humanChoice == "paper") {
+    rounds += 1
     if (computerChoice == "paper") {
       result.textContent = "Draw";
+      result.style.color = 'black'
     } else if (computerChoice == "scissors") {
       computerScore++;
       result.textContent = "Unluggy! Skazzors beats Pahper";
+      result.style.color = 'red'
     } else {
       humanScore++;
       result.textContent = "Noice! Pahper beats Rack";
+      result.style.color = 'green'
     }
   } else {
+    rounds += 1
     if (computerChoice == "scissors") {
       result.textContent = "Draw";
+      result.style.color = 'black'
     } else if (computerChoice == "rock") {
       computerScore++;
       result.textContent = "Unluggy! Rock beats Skazzors";
+      result.style.color = 'red'
     } else {
       humanScore++;
       result.textContent = "Noice! Skazzors beats Pahper";
+      result.style.color = 'green'
     }
   }
 }
